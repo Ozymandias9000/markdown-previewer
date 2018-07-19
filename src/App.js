1fdf7f24c2db2810adae3b4a61d6b194668a86a9
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-07-19 15:00:07 -0700

    Hooked up editor to preview, transforming markdown to HTML

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import Editor from "./components/Editor";
 import Previewer from "./components/Previewer";
 import "./static/css/App.css";
 
-const App = () => (
-  <Fragment>
-    <Editor />
-    <Previewer />
-  </Fragment>
-);
+export default class App extends Component {
+  state = {
+    markdown:
+      "## Welcome to my React Markdown Previewer! \n\n ### This is a sub - heading!!!"
+  };
 
-export default App;
+  handleChange = markdown => {
+    console.log(this.state);
+    this.setState({ markdown });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <Editor
+          handleChange={this.handleChange}
+          markdown={this.state.markdown}
+        />
+        <Previewer markdown={this.state.markdown} />
+      </Fragment>
+    );
+  }
+}
--- src/components/Previewer.jsx
+++ src/components/Previewer.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
+import marked from "marked";
 
 export default class Previewer extends Component {
+  markdownToHTML = () => {
+    const { markdown } = this.props;
+    document.getElementById("preview").innerHTML = marked(`${markdown}`);
+  };
+
+  componentDidMount() {
+    this.markdownToHTML();
+  }
+
+  componentDidUpdate() {
+    this.markdownToHTML();
+  }
+
   render() {
-    return <div id="preview">Preview</div>;
+    return (
+      <div id="preview--container">
+        Preview
+        <div id="preview" />
+      </div>
+    );
   }
 }
